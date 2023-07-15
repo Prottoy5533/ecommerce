78742213c19ac0a9d4aca7bc6534c39f5c94bdbd
Abid <abid5533@gmail.com>
2023-07-15 10:51:05 +0600

    project initialize

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context= context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound("The item is not available");
+            }
+            return product;
+        }
+    }
+}
